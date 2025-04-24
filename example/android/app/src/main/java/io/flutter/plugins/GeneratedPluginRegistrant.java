9e5e6db02872155e68bd86683dfcd5c9a5924262
Kawinda Jayasundara <hasith@mbizsoftware.com>
2025-04-24 14:39:29 +0530

    .

--- example/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ example/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -17,7 +17,7 @@ public final class GeneratedPluginRegistrant {
   public static void registerWith(@NonNull FlutterEngine flutterEngine) {
     try {
       flutterEngine.getPlugins().add(new io.crossingthestreams.flutterappauth.FlutterAppauthPlugin());
-    } catch(Exception e) {
+    } catch (Exception e) {
       Log.e(TAG, "Error registering plugin flutter_appauth, io.crossingthestreams.flutterappauth.FlutterAppauthPlugin", e);
     }
   }
